62caa5fa5ac760135e8c49e63f7e8942ae71746e
surbhisharma1581@gmail.com <surbhisharma1581@gmail.com>
2026-03-07 20:43:40 +0530

    first commit

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
-#include<iostream>
-using namespace std;    
-int main()    
-{    
-    int a, b, c;    
-    cout<<"Enter three numbers: ";    
-    cin>>a>>b>>c;    
-    if(a > b && a > c)    
-        cout<<a<<" is the greatest number.";    
-    else if(b > a && b > c)    
-        cout<<b<<" is the greatest number.";    
-    else if(c > a && c > b)    
-        cout<<c<<" is the greatest number.";    
-    else    
-        cout<<"All numbers are equal.";    
-    return 0;    
+#include<iostream>  
+using namespace std;
+int main()  
+{  
+    int a, b, c;  
+    cout<<"Enter three numbers: ";  
+    cin>>a>>b>>c;  
+    if(a>b && a>c)  
+        cout<<a<<" is the greatest number.";  
+    else if(b>a && b>c)  
+        cout<<b<<" is the greatest number.";  
+    else if(c>a && c>b)  
+        cout<<c<<" is the greatest number.";  
+    else
+        cout<<"All numbers are equal.";  
+    return 0;  
 }
